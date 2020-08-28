a92f7b8f5a5309f60f6259eae0d70802e3a3dc58
Ashknight99 <rachanaraut1999@gmail.com>
2020-08-28 22:30:59 +0530

    Day 20 commit

---
+++ Day 20.java
@@ -0,0 +1,48 @@
+/*
+Reorder List
+Given a singly linked list L: L0?L1?…?Ln-1?Ln,
+reorder it to: L0?Ln?L1?Ln-1?L2?Ln-2?…
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+Example 1:
+Given 1->2->3->4, reorder it to 1->4->2->3.
+Example 2:
+Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null)
+            return;
+        Stack <ListNode> stack = new Stack<>();
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode ptr = slow.next;
+        slow.next = null;
+        while(ptr != null){
+          stack.push(ptr);
+            ptr = ptr.next;
+        }
+        ListNode temp = null;
+        ptr = head;
+        while(!stack.isEmpty() && ptr!= null){
+            temp = stack.pop();
+            ListNode next = ptr.next;
+            ptr.next = temp;
+            temp.next = next;
+            ptr = next;
+        }                
+    }
+}
